0de215f3f27cfe260b91aa718e4e5a2b2cc140cf
Felipe <gomezfe@kean.edu>
2023-02-22 20:36:34 -0500

    Initial Commit

---
+++ Bridge1.0/Bridge1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Bridge1_0App.swift
+//  Bridge1.0
+//
+//  Created by Felipe Gomez on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Bridge1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
