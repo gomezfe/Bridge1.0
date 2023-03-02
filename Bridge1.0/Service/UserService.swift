3d6e4ab030ea6da09d448126f98c5775625a8a62
Felipe <gomezfe@kean.edu>
2023-03-02 16:49:18 -0500

    Creation of the Fetch User service, everything functional

--- Bridge1.0/Service/UserService.swift
+++ Bridge1.0/Service/UserService.swift
@@ -10,42 +10,22 @@ import FirebaseFirestoreSwift
 
 struct UserService {
     
-//    func fetchUser(withUid uid: String) {
+//    func fetchUser(withUid uid: String, completion: @escaping(User) -> Void) {
 //        Firestore.firestore().collection("users")
 //            .document(uid)
 //            .getDocument { snapshot, _ in
 //                guard let snapshot = snapshot else { return }
 //
 //                guard let user = try? snapshot.data(as: User.self) else { return }
+//                completion(user)
 //
-//                print("DEBUG: Username is \(user.username)")
-//                print("DEBUG: Email is \(user.email)")
+////                print("DEBUG: Username is \(user.username)")
+////                print("DEBUG: Email is \(user.email)")
 //            }
 //}
 
     
-    func fetchUser(withUid uid: String) {
-
-        let db = Firestore.firestore()
-        let userRef = db.collection("users").document(uid)
-
-        userRef.getDocument { (document, error) in
-            if let document =  document, document.exists {
-        // Convert document data to UserModel object
-                
-                
-                   guard let data = document.data() else { return }
-                
-                    
-                print("DEBUG: User data is \(data)")
-
-            }
-        }
-
-    }
-
-    
-//    func fetchUser(withUid uid: String, completion: @escaping (User?) -> Void) {
+//    func fetchUser(withUid uid: String) {
 //
 //        let db = Firestore.firestore()
 //        let userRef = db.collection("users").document(uid)
@@ -53,21 +33,49 @@ struct UserService {
 //        userRef.getDocument { (document, error) in
 //            if let document =  document, document.exists {
 //        // Convert document data to UserModel object
-//                do {
+//
+//                do{
+//
 //                    let data = try document.data(as: User.self)
-//                    print("DEBUG: User data from USerService is \(data)")
-//                    completion(data)
+//                    print("DEBUG: User data from USerService is \(data.username)")
+//                    print("DEBUG: User data from USerService is \(data.email)")
+//
+////                    print("DEBUG: User data is \(data)")
 //                } catch {
 //                    print("Error decoding user data: \(error.localizedDescription)")
-//                    completion(nil)
+//
 //                }
-//            } else {
-//                    print("User does not exist")
-//                    completion(nil)
 //            }
 //        }
 //
 //    }
+
+    
+    func fetchUser(withUid uid: String, completion: @escaping (User?) -> Void) {
+
+        let db = Firestore.firestore()
+        let userRef = db.collection("users").document(uid)
+
+        userRef.getDocument { (document, error) in
+            if let document =  document, document.exists {
+        // Convert document data to UserModel object
+                do {
+                    let data = try document.data(as: User.self)
+                    print("DEBUG: User data from USerService is \(data)")
+//                    print("DEBUG: User data from USerService is \(data.username)")
+//                    print("DEBUG: User data from USerService is \(data.username)")
+                    completion(data)
+                } catch {
+                    print("Error decoding user data: \(error.localizedDescription)")
+                    completion(nil)
+                }
+            } else {
+                    print("User does not exist")
+                    completion(nil)
+            }
+        }
+
+    }
 
 //func fetchUser(withID uid: String, completion: @escaping (UserModel?) -> Void) {
 //    let db = Firestore.firestore()
